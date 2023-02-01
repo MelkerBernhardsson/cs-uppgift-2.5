d261442c5c723b1967e4523614ae26faa34eadc3
Pingvu <nhlpool27@gmail.com>
2023-02-01 10:31:05 +0100

    Add project files.

---
+++ cs uppgift 2.5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace uppgift25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("skriv en multiplikation mellan två decimaltal.\n Exempel: 3,6*2,7");
+            string multiplikation = Console.ReadLine();
+            int multipliktecken = multiplikation.IndexOf("*");
+            string faktor1 = multiplikation[..multipliktecken];
+            string faktor2 = multiplikation[(multipliktecken+1)..];
+            float mathfaktor1 = float.Parse(faktor1);
+            float mathfaktor2 = float.Parse(faktor2);   
+            Console.WriteLine("svaret på ditt tal är:" + mathfaktor1 * mathfaktor2);
+        }
+    }
+}
